bcc635da249f1ab527aa8f003b6a7e0773f58877
ijayden-lung <ijayden.lung@gmail.com>
2021-09-30 11:29:38 +0300

    update

---
+++ projects/c2032/Split_BL6/STAR/split_strand.sh.2021093011.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#samtools view -b -f 128 -F 16 chr21.unique.bam > a.fwd1.bam
+#samtools view -b -f 80 chr21.unique.bam > a.fwd2.bam
+#samtools merge -f fwd.bam a.fwd1.bam a.fwd2.bam
+#samtools index fwd.bam
+bedtools genomecov -ibam fwd.bam -split -bg >fwd1.bedGraph
+#rm a.fwd*.bam
+
+
+#samtools view -b -f 144 chr21.unique.bam > a.rev1.bam
+#samtools view -b -f 64 -F 16 chr21.unique.bam > a.rev2.bam
+#samtools merge -f rev.bam a.rev1.bam a.rev2.bam
+#samtools index rev.bam
+bedtools genomecov -ibam rev.bam -split -bg >rev1.bedGraph
+#rm a.rev*.bam
+
+
